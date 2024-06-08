a8838118494c9db23c6effbf609c23e461d0a512
Poppyland937 <154359109+Poppyland937@users.noreply.github.com>
2024-06-08 17:23:02 +0100

    added api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 function submitSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector("#weather-city");
-  city.innerHTML = searchInput.value;
+  let cityPlace = document.querySelector("#weather-city");
+  let city = (cityPlace.innerHTML = searchInput.value);
+  fetchWeather(city);
 }
+function fetchWeather(city) {
+  let apiKey = "c48264o7b5ff7a9343004et42e6b4c41";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(displayTemperature);
+  console.log(url);
+}
+function displayTemperature(response) {
+  let temperatureNumber = document.querySelector("#weather-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+
+  temperatureNumber.innerHTML = temperature;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitSearch);
